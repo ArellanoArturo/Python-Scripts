48dfa400bd04256436d2805d1ee62d2d307d546c
Arturo Arellano <zomaveda@hotmail.com>
2026-06-04 02:35:10 -0600

    Remove files from repository

--- sweetvy_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sweetviz as sv
-
- # Load one specific sheet
-df = pd.read_excel(r"C:\Users\zomav\Downloads\CSO_KPI_Case.xlsx", sheet_name="Daily Log")
-df = df.loc[:, ~df.columns.str.startswith('Unnamed')]
-report = sv.analyze(df)
-report.show_html("my_report.html")  # opens automatically in your browser
